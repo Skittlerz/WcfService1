cbac24abfcd27d22a3ceac0e784f2da280938198
Amanda Braun <Ac_br_aun@hotmail.com>
2017-02-22 19:12:28 -0600

    initial commit

---
+++ WcfService1/IService1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+        [OperationContract]
+        string Welcome(string userName);
+
+        [OperationContract]
+        List<Movie> GetAllMovies();
+
+        [OperationContract]
+        List<Movie> GetMovieByTitle(String title);
+
+        [OperationContract]
+        List<Movie> GetMoviesByCategoryId(int catId);
+
+        [OperationContract]
+        List<MovieCategory> GetAllMovieCategories();
+
+        [OperationContract]
+        List<Stock> GetAllStocks();
+
+        [OperationContract]
+        List<Stock> GetStockByCode(string code);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    [DataContract]
+    public class Movie
+    {
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Director { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        public Movie() { }
+
+        public Movie(string title, string director, string descrip)
+        {
+            Title = title;
+            Director = director;
+            Description = descrip;
+        }
+    }
+
+    [DataContract]
+    public class MovieCategory
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int Id { get; set; }
+
+        public MovieCategory() { }
+
+        public MovieCategory(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    [DataContract]
+
+    public class Stock
+    {
+        [DataMember]
+        private string StockName { get; set; }
+
+        [DataMember]
+        private string StockCode { get; set; }
+
+        [DataMember]
+        private int Quantity { get; set; }
+
+        [DataMember]
+        private double Price { get; set; }
+
+        public Stock() { }
+
+        public Stock(string name, string code, int quantity, double price)
+        {
+            StockName = name;
+            StockCode = code;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Configuration;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public List<Movie> GetAllMovies()
+        {
+            List<Movie> movies = new List<Movie>();
+
+            SqlConnection con = 
+                new SqlConnection(WebConfigurationManager.ConnectionStrings["dbMovies"].ConnectionString);
+
+            SqlCommand cmd = new SqlCommand("Select Title, Director, Description FROM Movies");
+            cmd.Connection = con;
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Movie m = new WcfService1.Movie(reader["Title"].ToString(),
+                                                reader["Director"].ToString(),
+                                                reader["Description"].ToString());
+                movies.Add(m);
+            }
+
+            return movies;
+        }
+
+        public List<Movie> GetMovieByTitle(string title)
+        {
+            List<Movie> movies = new List<Movie>();
+
+            SqlConnection con =
+                new SqlConnection(WebConfigurationManager.ConnectionStrings["dbMovies"].ConnectionString);
+
+            SqlCommand cmd = new SqlCommand("Select Title, Director, Description FROM Movies Where Title=@title");
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("Title", title);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Movie m = new WcfService1.Movie(reader["Title"].ToString(),
+                                                reader["Director"].ToString(),
+                                                reader["Description"].ToString());
+                movies.Add(m);
+            }
+
+            return movies;
+        }
+
+        public List<MovieCategory> GetAllMovieCategories()
+        {
+            List<MovieCategory> movieCats = new List<MovieCategory>();
+
+            SqlConnection con =
+                new SqlConnection(WebConfigurationManager.ConnectionStrings["dbMovies"].ConnectionString);
+
+            SqlCommand cmd = new SqlCommand("Select Id, Name FROM MovieCategories");
+            cmd.Connection = con;
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                MovieCategory m = new WcfService1.MovieCategory(Convert.ToInt32(reader["Id"].ToString()), 
+                    reader["Name"].ToString());
+                movieCats.Add(m);
+            }
+
+            return movieCats;
+        }
+
+        public List<Movie> GetMoviesByCategoryId(int catId)
+        {
+            List<Movie> movies = new List<Movie>();
+
+            SqlConnection con =
+                new SqlConnection(WebConfigurationManager.ConnectionStrings["dbMovies"].ConnectionString);
+
+            SqlCommand cmd = new SqlCommand("Select Title, Director, Description FROM Movies Where CategoryId=@catId");
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("catId", catId);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Movie m = new WcfService1.Movie(reader["Title"].ToString(),
+                                                reader["Director"].ToString(),
+                                                reader["Description"].ToString());
+                movies.Add(m);
+            }
+
+            return movies;
+        }
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string Welcome(string user)
+        {
+            return string.Format("Hello {0}, welcome to the world of web services!",user);
+        }
+
+        public List<Stock> GetAllStocks()
+        {
+            List<Stock> stocks = new List<Stock>();
+
+            SqlConnection con =
+                new SqlConnection(WebConfigurationManager.ConnectionStrings["dbStocks"].ConnectionString);
+
+            SqlCommand cmd = new SqlCommand("Select StockName, StockSymbol, Shares, Price FROM Stocks");
+            cmd.Connection = con;
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Stock s = new WcfService1.Stock(reader["StockName"].ToString(),
+                                                reader["StockSymbol"].ToString(),
+                                                Convert.ToInt32(reader["Shares"]),
+                                                Convert.ToDouble(reader["price"]));
+                stocks.Add(s);
+            }
+
+            return stocks;
+        }
+
+        public List<Stock> GetStockByCode(string code)
+        {
+            List<Stock> stocks = new List<Stock>();
+
+            SqlConnection con =
+                new SqlConnection(WebConfigurationManager.ConnectionStrings["dbStocks"].ConnectionString);
+
+            SqlCommand cmd = new SqlCommand("Select StockName, StockSymbol, Shares, Price FROM Stocks Where StockSymbol=@code");
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("code", code);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Stock s = new WcfService1.Stock(reader["StockName"].ToString(),
+                                                reader["StockSymbol"].ToString(),
+                                                Convert.ToInt32(reader["Shares"]),
+                                                Convert.ToDouble(reader["price"]));
+
+                stocks.Add(s);
+            }
+
+            return stocks;
+        }
+    }
+}
